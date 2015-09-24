315d54cc8e90e91be07680c1948d0a87e30c177f
jitendray <jeetu.yadav28@gmail.com>
2015-09-24 07:59:59 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="CSS/jquery.dataTables.min.css" media="screen">
+        <title></title>
+    </head>
+    <body>
+        <table id="issueTable" class="display" width="100%"></table>
+    </body>
+    <script src="JS/jquery.js"></script>
+    <script src='JS/jquery.dataTables.min.js'></script>
+    <script src='JS/IssuesTable.js'></script>
+</html>
